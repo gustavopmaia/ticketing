db5f61f51da44772a3af166a046a24c181f5918b
Gustavo Maia <gustavopmaia@hotmail.com>
2025-03-13 17:59:21 -0300

    tests

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,27 +7,26 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined')
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined')
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined')
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
   try {
-    if (!process.env.JWT_KEY) {
-      throw new Error('JWT_KEY must be defined')
-    }
-
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI must be defined')
-    }
-
-    if (!process.env.NATS_URL) {
-      throw new Error('NATS_URL must be defined')
-    }
-
-    if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error('NATS_CLUSTER_ID must be defined')
-    }
-
-    if (!process.env.NATS_CLIENT_ID) {
-      throw new Error('NATS_CLIENT_ID must be defined')
-    }
-
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
